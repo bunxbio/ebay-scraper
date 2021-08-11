a7d2206f34c200b139eabae2e53c94930639c91b
Bio Kenfi Gumilang <biokenfigumilang@gmail.com>
2021-08-12 03:25:37 +0700

    updating parsers for listings page

---
+++ parsers/listings.rb
@@ -0,0 +1,14 @@
+html = Nokogiri.HTML(content)
+
+listings = html.css('.b-list__items_nofooter .s-item')
+
+listings.each do |listing|
+	product = {}
+
+	product['title'] = listing.css('.s-item__title')&.text
+	product['price'] = listing.css('.s-item__price')&.text
+	product['url'] = listing.css('.s-item__link').attr('href')&.text
+	product['_collection'] = "listings"
+
+	outputs << product
+end
