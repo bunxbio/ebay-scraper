2ab0f0a7e6acf3f1cb1899f32ffba14f4f408cfb
Bio Kenfi Gumilang <biokenfigumilang@gmail.com>
2021-08-13 04:38:26 +0700

    fixing parser for details.rb

--- parsers/details.rb
+++ parsers/details.rb
@@ -1,7 +1,7 @@
 html = Nokogiri.HTML(content)
 
 seller = html.css('.si-content .bdg-90 .mbg .mbg-nw')&.text
-seller = html.css('.seller-persona .no-wrap')[1]&.text if seller.strip == ''
+seller = html.css('.seller-persona a')[0]&.text if seller.strip == ''
 raise 'something happened on seller!' if seller.strip == ''
 
 feedback = html.css('.bdg-90 #si-fb')&.text
