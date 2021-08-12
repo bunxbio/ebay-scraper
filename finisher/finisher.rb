d4d1ab6763762814d6cdcab9e0ba6026dafab0a7
Bio Kenfi Gumilang <biokenfigumilang@gmail.com>
2021-08-13 06:31:05 +0700

    adding Gemfile, Gemfile.lock, dh_easy.yaml, and modifying finisher.rb

--- finisher/finisher.rb
+++ finisher/finisher.rb
@@ -1,3 +1,5 @@
+require "dh_easy/qa"
+
 collections = Datahen::Client::ScraperJobOutput.new.collections("ebay-scraper")
 collection = collections.find{|collection| collection['collection'] == "listings" }
 if collection
@@ -9,3 +11,6 @@ if collection
 else
    puts "no listings collection found"
 end
+
+vars = { "scraper_name" => "ebay-scraper", "collections" => ["listings"]}
+DhEasy::Qa::Validator.new.validate_internal(vars, outputs)
