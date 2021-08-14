63f5427fe49b7303e1be0df78384545552e6ee21
Bio Kenfi Gumilang <biokenfigumilang@gmail.com>
2021-08-14 08:13:06 +0700

    modifying finisher.rb

--- finisher/finisher.rb
+++ finisher/finisher.rb
@@ -1,5 +1,3 @@
-require "dh_easy/qa"
-
 collections = Datahen::Client::ScraperJobOutput.new.collections("ebay-scraper")
 collection = collections.find{|collection| collection['collection'] == "listings" }
 if collection
@@ -11,6 +9,3 @@ if collection
 else
    puts "no listings collection found"
 end
-
-vars = { "scraper_name" => "ebay-scraper", "collections" => ["listings"]}
-DhEasy::Qa::Validator.new.validate_internal(vars, outputs)
